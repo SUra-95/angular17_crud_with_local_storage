f318a54c174097afff73f82c98b57d6d24d4b724
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-09-16 11:21:09 +0530

    Insert records to the local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_17_crud_with_localstorage';
 
   @ViewChild('myModal') modal: ElementRef | undefined;
+  studentObj: Student = new Student();
+  studentList: Student[] = [];
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem('angular17crud');
+    if (localData != null) {
+      this.studentList = JSON.parse(localData);
+    }
+  }
 
   openModal() {
     const modal = document.getElementById('myModal');
-    if( modal!= null ) {
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
@@ -24,5 +35,43 @@ export class AppComponent {
     if (this.modal && this.modal.nativeElement) {
       this.modal.nativeElement.style.display = 'none';
     }
+  }
+
+  saveStudent() {
+    const isLocalpresent = localStorage.getItem('angular17crud');
+    if (isLocalpresent != null) {
+      const oldArray = JSON.parse(isLocalpresent);
+      oldArray.push(this.studentObj);
+      localStorage.setItem('angular17crud', JSON.stringify(oldArray));
+      this.studentList = oldArray;
+    } else {
+      const newArr = [];
+      newArr.push(this.studentObj);
+      localStorage.setItem('angular17crud', JSON.stringify(newArr));
+      this.studentList = newArr;
+    }
+    this.studentObj = new Student();
+    this.closeModal();
+  }
+}
+
+
+export class Student {
+  name: string;
+  mobileNo: string;
+  email: string;
+  city: string;
+  state: string;
+  pincode: string;
+  address: string;
+
+  constructor() {
+    this.name = '';
+    this.mobileNo = '';
+    this.email = '';
+    this.city = '';
+    this.state = '';
+    this.pincode = '';
+    this.address = '';
   }
 }
